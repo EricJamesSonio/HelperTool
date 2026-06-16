83552768fc273d89a1e70654f375ed41a2ee0fcb
EricSonio <ericjamessonio7@gmail.com>
2026-06-17 05:48:49 +0800

    remove tester

--- test_loader.js
+++
@@ -1,9 +0,0 @@
-﻿const rulesLoader = require('./Code/ipc/blueprintLibrary/rulesLoader.js');
-const data = rulesLoader.loadRulesData();
-console.log('Categories:');
-data.categories.forEach(c => console.log('  -', c.name, '(' + c.type + ')'));
-console.log('\nBlueprints:');
-for (const [cat, bps] of Object.entries(data.blueprints)) {
-  console.log('  ' + cat + ':');
-  bps.forEach(b => console.log('    - "' + b.name + '" |', b.description.slice(0, 70)));
-}
